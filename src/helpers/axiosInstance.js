613cdaa885d292e4b74e78959d7773c783deb25b
Md Parvez <flparvez23@gmail.com>
2024-02-15 23:12:06 +0600

    solved ?

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,15 +1,16 @@
 "use client"
 import axios from "axios";
-import {BASE_URL} from "../constants.js"
+import { BASE_URL } from "../constants.js"
 
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
 axiosInstance.defaults.withCredentials = true;
-axiosInstance.defaults.headers={
-     'Access-Control-Allow-Origin': true,
+axiosInstance.defaults.headers = {
+    'Access-Control-Allow-Origin': 'https://yt-backned.vercel.app/',
     // XMLHttpRequest:true,
     
+
 }
 
 
